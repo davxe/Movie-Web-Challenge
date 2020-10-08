e2c38b429392e1d93a56254ccfde22161fb3f095
davxe <princekaushal14918177h@gmail.com>
2020-10-08 10:43:30 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter,Route,Link,Switch} from 'react-router-dom'
+import {BrowserRouter,Route,Link} from 'react-router-dom'
 import MovieList from './MovieList'
 import MovieShow from './MovieDetail'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,7 +9,7 @@ function App(props)
     return(
         <BrowserRouter>
             <div>
-                <Link to='/'></Link>
+                <Link to='/'>List</Link>
                     <Route path='/' component={MovieList} exact={true}/>
                     <Route path='/movie/:imdbID' component={MovieShow}/>
             </div>
