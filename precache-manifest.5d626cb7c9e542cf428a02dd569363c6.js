61fa1227dcf607476a1f72d1af10a3fd213e83f6
davxe <princekaushal14918177h@gmail.com>
2020-10-08 09:27:13 +0530

    Updates

---
+++ precache-manifest.5d626cb7c9e542cf428a02dd569363c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e5058f930cb06b15351f52b2a5a3e4",
+    "url": "/Movie-Web-Challenge/index.html"
+  },
+  {
+    "revision": "127e38cc3eaf2dfa5a76",
+    "url": "/Movie-Web-Challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ffaaa6d71a6e65962d2d",
+    "url": "/Movie-Web-Challenge/static/css/main.612ae3ca.chunk.css"
+  },
+  {
+    "revision": "127e38cc3eaf2dfa5a76",
+    "url": "/Movie-Web-Challenge/static/js/2.cb146f9d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Movie-Web-Challenge/static/js/2.cb146f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffaaa6d71a6e65962d2d",
+    "url": "/Movie-Web-Challenge/static/js/main.e6406b2b.chunk.js"
+  },
+  {
+    "revision": "01d246da9fc0cfb60359",
+    "url": "/Movie-Web-Challenge/static/js/runtime-main.eb353966.js"
+  }
+]);
